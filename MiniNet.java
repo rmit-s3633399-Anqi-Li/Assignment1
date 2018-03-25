c4255b0f17e260f603c3bfaebfbb36eb9606e547
rmit-s3633399-Anqi-Li <31309552+rmit-s3633399-Anqi-Li@users.noreply.github.com>
2018-03-25 18:50:33 +1100

    Add files via upload

---
+++ MiniNet.java
@@ -0,0 +1,19 @@
+
+/**
+ * @author Anqi Li s3633399
+ * main startup class
+ *
+ */
+public class MiniNet {
+
+	public MiniNet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Driver().start();
+
+	}
+
+}
